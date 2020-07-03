36b32233ce3228734549e4b11288e99c1d52b91b
Gabriela.Piovezan <gabrielapiovezan@live.com>
2020-07-03 18:01:18 -0300

    Pegando arquivos com extensão md

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 module.exports = () => {
-  // ...
+    // ...
 };
+
+var path = require("path");
+var fs = require("fs");
+
+function getFiles(dir, files_, fileType) {
+    var regex = fileType ? new RegExp("\\" + fileType + "$") : "";
+
+    return fs.readdirSync(dir).reduce(function(allFiles, file) {
+        var name = path.join(dir, file);
+        if (fs.statSync(name).isDirectory()) {
+            getFiles(name, allFiles, fileType);
+        } else if (file.match(regex)) {
+            allFiles.push(name);
+        }
+        return allFiles;
+    }, files_ || []);
+}
+const files = getFiles("./", "", ".md");
+//console.log(files);
